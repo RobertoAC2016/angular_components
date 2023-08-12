be0c3d8d223c25bb227ef6e19f4c9f97f8e43fcf
Roberto Avila <ravila@dealergeek.com>
2023-08-12 15:16:08 -0600

    Uso de las directivas ngif y ngfor

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { users } from '../../shared/data';
+
 
 @Component({
   selector: 'app-pages',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent {
+  // definimos la variable de tipo global y publica ya que sino la hacemos publica no la
+  // podremos usar fuera del archivo TS y la necesitaremos en el HTML en su template
+  // veamos como es esto, cambiemos a private y vemos q pasa
+  // este al ser un objeto como tal no me deja inicializarlo como clase, ya se usa directamente
+  public items = users;
+/**
+ * vamos a trabajar con las directivas ngIf y ngFor de angular para la toma de
+ * desiciones y ciclos en el html y no es como codigo JS y TS aunque tamben
+ * vamos a ver ejemplos en el archivo TS del componente, usaremos pages como
+ * nuestro template principal
+ * **/
+  // generamos el constructor
+  constructor(){
+    // aqui inicializaremos el objeto dentro de TS para poder ver como se carga un
+    // objeto en este archivo con un objeto de tipo array de diferentes elementos
+
+    //con este foreach recorremos cada elemento de la coleccion
+    // this.items.forEach((ele) => {
+    //   console.log(ele);
+    // });
+    // vamos a usarlo fuera del TS en el HTML
+  }
 
 }
---
+++ src/app/shared/data.ts
@@ -0,0 +1,52 @@
+export const users = [
+  {
+    "nombre": "Ejemplo 1",
+    "edad": 25,
+    "activo": true
+  },
+  {
+    "nombre": "Ejemplo 2",
+    "edad": 30,
+    "activo": false
+  },
+  {
+    "nombre": "Ejemplo 3",
+    "edad": 22,
+    "activo": true
+  },
+  {
+    "nombre": "Ejemplo 4",
+    "edad": 28,
+    "activo": false
+  },
+  {
+    "nombre": "Ejemplo 5",
+    "edad": 35,
+    "activo": true
+  },
+  {
+    "nombre": "Ejemplo 6",
+    "edad": 29,
+    "activo": true
+  },
+  {
+    "nombre": "Ejemplo 7",
+    "edad": 31,
+    "activo": false
+  },
+  {
+    "nombre": "Ejemplo 8",
+    "edad": 27,
+    "activo": true
+  },
+  {
+    "nombre": "Ejemplo 9",
+    "edad": 24,
+    "activo": false
+  },
+  {
+    "nombre": "Ejemplo 10",
+    "edad": 33,
+    "activo": true
+  }
+];
