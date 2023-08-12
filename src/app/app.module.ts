06818c1c8b904bad12bc64ffed092bc90aeacb7d
Roberto Avila <ravila@dealergeek.com>
2023-08-12 13:07:47 -0600

    Uso final de como se utilizan los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { HeaderComponent } from './components/pages/header/header.component';
+import { BodyComponent } from './components/pages/body/body.component';
+import { FooterComponent } from './components/pages/footer/footer.component';
+import { LeftComponent } from './components/pages/body/left/left.component';
+import { RightComponent } from './components/pages/body/right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
